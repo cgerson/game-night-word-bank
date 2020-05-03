6cb71af8672537e8499d74e88a6072318cc859b2
Claire Gerson <claire.gerson@gotinder.com>
2020-05-03 16:56:46 -0700

    custom cards

--- app.py
+++ app.py
@@ -9,47 +9,96 @@ from flask import Flask, render_template, request
 # https://hackersandslackers.com/redis-py-python/
 
 app = Flask(__name__)
-redis_url = os.environ['REDISCLOUD_URL']
+debug = True
+if debug: 
+    redis_url= "redis://rediscloud:Fc4aIcY1b8uIcFIDDIkVCguFYO3rU0dI@redis-18892.c232.us-east-1-2.ec2.cloud.redislabs.com:18892"
+else:
+    redis_url = os.environ['REDISCLOUD_URL']
 
 db=redis.from_url(redis_url)
+db.set('default_card_pack', 'Oprah_Santa Claus_Harry Potter_Beyonce_Lance Armstrong')
+db.set('custom_card_pack', '', 14400) # 4 hour ttl
 db.set('cards_remaining', 'Oprah_Santa Claus_Harry Potter_Beyonce_Lance Armstrong')
+db.set('card_pack', 'default_card_pack')
+card_pack_labels = {
+    'default_card_pack': 'Default',
+    'custom_card_pack': 'Custom'
+}
 
 @app.route('/')
 def hello_world():
     numcards = get_numcards()
-    return render_template('index.html', numcards = numcards)
+    current_card_pack = get_current_card_pack()
+    return render_template('index.html', numcards = numcards, card_pack_label = current_card_pack)
 
+# set back cards remaining to chosen cardpack
 @app.route('/reset/')
 def reset():
-    db.set('cards_remaining', 'Oprah_Santa Claus_Harry Potter_Beyonce_Lance Armstrong')
+    cardpack = db.get('card_pack')
+    cards = db.get(cardpack).decode('UTF-8')
+    db.set('cards_remaining', cards)
     numcards = get_numcards()
-    return render_template('index.html', numcards = numcards)
-    #return 'Cards reset.'
+    current_card_pack = get_current_card_pack()
+    return render_template('index.html', numcards = numcards, card_pack_label = current_card_pack)
 
+# add cards to default or custom cardpack
 @app.route('/update/', methods = ['POST', 'GET'])
 def update():
-    cards=db.get('cards_remaining').decode('UTF-8')
+    #cardpack = db.get('card_pack')
+    #cards=db.get(cardpack).decode('UTF-8')
+    cards=db.get('custom_card_pack').decode('UTF-8')
     newcard = request.form.get("newcard")
     cards = cards + "_" + str(newcard)
-    db.set('cards_remaining',cards)
+    db.set('custom_card_pack',cards)
     return render_template("addcard.html", result = "Card added: {0}".format(newcard))
 
 @app.route('/add/', methods = ['POST', 'GET'])
 def add():
     return render_template("addcard.html")
 
+# select cardpack (should be selected AFTER adding cards)
+@app.route('/updatecardpack/', methods = ['POST', 'GET'])
+def updatecardpack():
+    #cards=db.get('cards_remaining').decode('UTF-8')
+    cardpack = request.form.get('cardpack')
+    cardpack_label = card_pack_labels.get(cardpack)
+    db.set('card_pack',cardpack) # set to chosen cardpack
+    cards = db.get(cardpack).decode('UTF-8')
+    db.set('cards_remaining', cards) # set chosen cardpack to remaining cards
+    return render_template("cardpack.html", result = "Card Pack Selected: {0}".format(cardpack_label), checked = cardpack_label)
+
+@app.route('/setcardpack/', methods = ['POST', 'GET'])
+def setcardpack():
+    current_card_pack = get_current_card_pack()
+    return render_template("cardpack.html", checked = current_card_pack)
+
+# pick cards from a temp set, so that card list remains for reset
 @app.route('/pickcard/')
 def pickcard():
-    cards = db.get('cards_remaining').decode('UTF-8').split("_")
-    card = random.choice(cards)
-    cards.remove(card)
-    numcards = len(cards)
-    cards_remaining = "_".join(cards)
-    db.set('cards_remaining', cards_remaining)
-    return render_template('index.html', card=card, numcards = numcards)
+    cards_raw = db.get('cards_remaining').decode('UTF-8').split("_")
+    cards = list(filter(lambda x: x, cards_raw))
+
+    numcards = 0
+    current_card_pack = get_current_card_pack()
+
+    if len(cards) > 0:
+        card = random.choice(cards)
+        cards.remove(card)
+        numcards = len(cards)
+        cards_remaining = "_".join(cards)
+        db.set('cards_remaining', cards_remaining)
+    else:
+        card = "CLICK RESET TO BEGIN NEXT ROUND"
+
+    return render_template('index.html', card = card, numcards = numcards, card_pack_label = current_card_pack)
+
+def get_current_card_pack():
+    current_card_pack = db.get('card_pack').decode('UTF-8')
+    return card_pack_labels.get(current_card_pack)
 
 def get_numcards():
-    cards=str(db.get('cards_remaining')).split("_")
+    cards_raw=db.get('cards_remaining').decode('UTF-8').split("_")
+    cards = list(filter(lambda x: x, cards_raw))
     return len(cards)
 
 if __name__ == '__main__':
