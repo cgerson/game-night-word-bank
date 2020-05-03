50338228b1b2542601d6afa3443e932179af86b9
Claire Gerson <claire.gerson@gotinder.com>
2020-05-02 23:57:40 -0700

    so good

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from __future__ import unicode_literals
+
 import os
 import random
 import redis
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # http://clouddatafacts.com/heroku/heroku-flask-redis/flask_redis.html#pre-rerequisites 
 # https://hackersandslackers.com/redis-py-python/
@@ -14,7 +16,7 @@ else:
     redis_url = os.environ['REDISCLOUD_URL']
 
 db=redis.from_url(redis_url)
-db.set('cards_remaining', "Oprah_Santa Claus_Harry Potter")
+db.set('cards_remaining', 'Oprah_Santa Claus_Harry Potter_Beyonce_Lance Armstrong')
 
 @app.route('/')
 def hello_world():
@@ -23,19 +25,26 @@ def hello_world():
 
 @app.route('/reset/')
 def reset():
-    db.set('cards_remaining', "Oprah_Santa Claus_Harry Potter")
-    return 'Cards reset.'
+    db.set('cards_remaining', 'Oprah_Santa Claus_Harry Potter_Beyonce_Lance Armstrong')
+    numcards = get_numcards()
+    return render_template('index.html', numcards = numcards)
+    #return 'Cards reset.'
 
-@app.route('/add/<newcard>')
-def add(newcard):
+@app.route('/update/', methods = ['POST', 'GET'])
+def update():
     cards=str(db.get('cards_remaining'))
+    newcard = request.form.get("newcard")
     cards = cards + "_" + str(newcard)
     db.set('cards_remaining',cards)
-    return 'Cards updated.'
+    return render_template("addcard.html", result = "Card added: {0}".format(newcard))
 
-@app.route('/pickcard/', methods=['POST', 'GET'])
+@app.route('/add/', methods = ['POST', 'GET'])
+def add():
+    return render_template("addcard.html")
+
+@app.route('/pickcard/')
 def pickcard():
-    cards=str(db.get('cards_remaining')).split("_")
+    cards = db.get('cards_remaining').decode('UTF-8').split("_")
     card = random.choice(cards)
     cards.remove(card)
     numcards = len(cards)
